0a311a56481de3436d4ea122006583abbf8ea448
vishal delwadiya <vishal@tmtechindia.co>
2022-10-04 02:53:12 +0530

    - add get and post data using many ways like (json serialisation, encodable-decodable protocol) in ThirdVC file. bath to code cat 15 example and implement.

--- PushNotification_DemoPractice/Controller/ThirdVC.swift
+++ PushNotification_DemoPractice/Controller/ThirdVC.swift
@@ -9,21 +9,64 @@ import UIKit
 
 class ThirdVC: UIViewController {
 
+    var aryResult: [Result] = [Result]()
+    var aryDisplayMore: [Result] = [Result]()
+    
+    let topAlbumViewModel: TopAlbumViewModel = TopAlbumViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let strUrl = Constant.BASEAPI
+        let url = URL(string: strUrl)
+        
+        //getApiData(url: url!)
+        
+        postApiData(url: url!)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // CodeCat Api Example
+    //MARK: - JSONSerialization GET Method Use
+    // read data using json serialization using url session (old fashion passing)
+    func getApiData(url: URL) {
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (serviceData, serviceResponse, error) in
+            
+            if error == nil {
+                let httpResponse = serviceResponse as! HTTPURLResponse
+                
+                print(httpResponse.statusCode)
+                // 200 means success result
+                if httpResponse.statusCode == 200 {
+                    
+                    let jsonData = try? JSONSerialization.jsonObject(with: serviceData!, options: .mutableContainers)
+                    print(jsonData)
+                }
+            }
+        }
+        task.resume()
     }
-    */
+
+    //MARK: - JSONSerialization POST Method & URLRequest using get method
+    func postApiData(url: URL) {
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "get"
+        
+        //let dict = ["userId":"1","password":"1234"]
+        //let requestBody = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted)
+        //request.httpBody = requestBody
+        //request.addValue("application/json", forHTTPHeaderField: "content-type")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if (error == nil && data != nil && data?.count != 0) {
+                
+                let result = String(data: data!, encoding: .utf8)
+                debugPrint(result!)
+            }
+        }.resume()
+    }
 
 }
